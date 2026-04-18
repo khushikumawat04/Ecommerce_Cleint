0bd17e8f538ecc82af9ab95421d6e65116437b66
Khushi Kumawat <127881708+khushikumawat04@users.noreply.github.com>
2026-04-18 14:12:58 +0530

    Fix case sensitivity in import paths

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const Order= require("../models/Orders");
-const sendEmail = require("../utils/sendEmail");
+const sendEmail = require("../Utils/sendEmail");
 const User = require("../models/User");
 const  formatDate  = require("../Utils/FormateDate");
 // Protected route
